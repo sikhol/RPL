7f9196c517f3c596d242a07e7b52e1fc26acde1f
emfauzanashari <fauzanashari29@gmail.com>
2018-12-02 20:58:37 +0700

    Rpl first

---
+++ app/src/main/java/com/example/emfauzanashari/nyoba/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.emfauzanashari.nyoba;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView Home = (TextView) findViewById(R.id.btn_login);
+        Home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/emfauzanashari/nyoba/MkActivity.java
@@ -0,0 +1,13 @@
+package com.example.emfauzanashari.nyoba;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mk);
+    }
+}
